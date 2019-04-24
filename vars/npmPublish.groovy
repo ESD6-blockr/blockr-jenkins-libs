9f9b5811f4955570cae19317d7e1ef526f6be955
Roel van den Boom <roelvandenboom@inogo.nl>
2019-04-24 11:18:07 +0200

    Fix npm publish stages

--- vars/npmPublish.groovy
+++ vars/npmPublish.groovy
@@ -1,33 +1,30 @@
 #!/usr/bin/groovy
 
 def call(Map settings) {
-    stage('Publish package') {
-        parallel development: {
-            if (env.BRANCH_NAME == 'develop') {
-                echo 'Publishing to dev environemnt'
-                npmPublishStages('dev')
-                archive(settings.archive_folders)
-                logToJira("Published", "develop")
-                return;
-            }
-        },
-        staging: {
-            if (env.BRANCH_NAME.contains('release')) {
-                echo 'Publishing to staging environemnt'
-                npmPublishStages('staging')
-                archive(settings.archive_folders)
-                logToJira("Published", "staging")
-                return;
-            }
-        },
-        producion: {
-            if (env.BRANCH_NAME == 'master') {
-                echo 'Publishing to production environemnt'
-                npmPublishStages()
-                archive(settings.archive_folders)
-                logToJira("Published", "production")
-                return;
-            }
+    if (env.BRANCH_NAME == 'develop') {
+        stage('Publish development package') {
+            echo 'Publishing to dev environemnt'
+            npmPublishStages('dev')
+            archive(settings.archive_folders)
+            logToJira("Published", "develop")
+        }
+    }
+
+    if (env.BRANCH_NAME == 'release') {
+        stage('Publish staging package') {
+            echo 'Publishing to staging environemnt'
+            npmPublishStages('staging')
+            archive(settings.archive_folders)
+            logToJira("Published", "staging")
+        }
+    }
+
+    if (env.BRANCH_NAME == 'master') {
+        stage('Publish production package') {
+            echo 'Publishing to production environemnt'
+            npmPublishStages('production')
+            archive(settings.archive_folders)
+            logToJira("Published", "production")
         }
     }
 }
