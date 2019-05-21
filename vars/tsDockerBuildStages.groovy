44d14af9094c1dd3cfce0a0300af920e1f1a2579
Roel van den Boom <roelvandenboom@inogo.nl>
2019-05-21 20:20:07 +0200

    Fix volume for retrieving coverage report jest

--- vars/tsDockerBuildStages.groovy
+++ vars/tsDockerBuildStages.groovy
@@ -34,6 +34,7 @@ def call(String repo, Map settings) {
 
     stage('Test') {
         sh 'mkdir coverage'
+        sh 'docker run -v "$PWD/coverage":/opt/coverage ${testImageName}'
         sh "docker run --rm -v ${env.WORKSPACE}/coverage:/opt/coverage ${testImageName}"
     }
 
