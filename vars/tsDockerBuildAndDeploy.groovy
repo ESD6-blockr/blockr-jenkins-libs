de4fb485faacffaa6f80f4caea477a1f471bcf06
Dane Naebers <danenaebers@gmail.com>
2019-05-21 13:29:43 +0200

    add docker build pipeline parts

---
+++ vars/tsDockerBuildAndDeploy.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/groovy
+
+def call(String repo, Map settings) {
+    node('docker') {
+        try {
+            scmClone()
+
+            getVersion('npm')
+
+            tsDockerBuildStages(repo, settings)
+        }
+        catch(all) {
+            currentBuild.result = 'FAILURE'
+        }
+        finally {
+            cleanWorkSpace()
+        }
+    }
+}
---
+++ vars/tsDockerBuildStages.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/groovy
+
+def call(String repo, Map settings) {
+    def version = env.PROJECT_VERSION
+    def deployImageName
+    def testImageName
+
+
+    stage('Initialize') {
+        def branch = env.BRANCH_NAME
+       
+        if (branch == 'develop' || branch.contains('feature')) {
+            String content =  '@blockr:registry=https://npm-dev.naebers.me'
+                           
+            writeFile(file: ".npmrc", text: content, encoding: "UTF-8")
+        }
+
+        if (branch.contains('release')) {
+            String content =  '@blockr:registry=https://npm-staging.naebers.me'
+                           
+            writeFile(file: ".npmrc", text: content, encoding: "UTF-8")        
+        }
+    }
+
+    stage('Build') {
+        deployImageName = "${repo}:${version}"
+        testImageName = "${repo}-test:${version}"
+
+        sh "docker build -t ${deployImageName} --build-arg 'VERSION='${version}' . "
+        sh "docker build --target test -t ${testImageName} --build-arg 'VERSION=${version}' . "
+    }
+
+    stage('Test') {
+        fileOperations([folderCreateOperation('coverage')])
+
+        sh "docker run --rm -v ${env.WORKSPACE}/coverage:/coverage ${testImageName}"
+    }
+
+    stage('Record results') {
+        step([$class: 'CoberturaPublisher', coberturaReportFile: 'coverage/cobertura-coverage.xml'])
+    }
+
+    if (settings.sonar_key != null) {
+       tsSonarScan(settings.sonar_key, settings.source_folder);
+
+       awaitSonarResults()
+    }
+}
