6ab90807688be9352ca1d077bea71eb0ebd72cb0
Dane Naebers <danenaebers@gmail.com>
2019-06-05 22:47:14 +0200

    debug

--- vars/awaitSonarResults.groovy
+++ vars/awaitSonarResults.groovy
@@ -7,9 +7,12 @@ def call() {
                 def qg = waitForQualityGate()
                 if (qg.status != 'OK') {
                     echo "The Build will be marked unstable due to quality gate failure: ${qg}"
+                    echo '1'
                     currentBuild.result = 'UNSTABLE'
+                    echo '2'
                 }
             } catch (all) {
+                echo '3'
                 currentBuild.result = 'UNSTABLE'
             }
         }
