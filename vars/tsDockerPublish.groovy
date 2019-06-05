bacd52825866e6305b59c77664713638ea049ae3
Dane Naebers <danenaebers@gmail.com>
2019-06-05 23:22:28 +0200

    fix syntax error

--- vars/tsDockerPublish.groovy
+++ vars/tsDockerPublish.groovy
@@ -6,11 +6,11 @@ def call(String repo, ArrayList archive_folders = null) {
     def version = env.PROJECT_VERSION
 
     if (branch == 'develop' || branch.contains('feature') || branch.contains('fix')) {
-        tsDockerPublishStages(imageName, repo 'develop', archive_folders)
+        tsDockerPublishStages(imageName, repo, 'develop', archive_folders)
     }
 
     if (branch.contains('release')) {
-        tsDockerPublishStages(imageName, repo 'staging', archive_folders)
+        tsDockerPublishStages(imageName, repo, 'staging', archive_folders)
     }
 
     tsDockerPublishStages(imageName, repo, null, archive_folders)
