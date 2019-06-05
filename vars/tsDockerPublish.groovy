6e722362707a8a0742f431f2adc5624a1041fbb5
Dane Naebers <danenaebers@gmail.com>
2019-06-05 23:28:34 +0200

    fix call signature

--- vars/tsDockerPublish.groovy
+++ vars/tsDockerPublish.groovy
@@ -6,12 +6,12 @@ def call(String repo, ArrayList archive_folders = null) {
     def version = env.PROJECT_VERSION
 
     if (branch == 'develop' || branch.contains('feature') || branch.contains('fix')) {
-        tsDockerPublishStages(imageName, repo, 'develop', archive_folders)
+        tsDockerPublishStages(imageName, repo, 'develop', version, archive_folders)
     }
 
     if (branch.contains('release')) {
-        tsDockerPublishStages(imageName, repo, 'staging', archive_folders)
+        tsDockerPublishStages(imageName, repo, 'staging', version, archive_folders)
     }
 
-    tsDockerPublishStages(imageName, repo, null, archive_folders)
+    tsDockerPublishStages(imageName, repo, null, version, archive_folders)
 }
